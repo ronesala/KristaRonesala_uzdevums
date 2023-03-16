1ef3348a2dd1dab8e297d7f61dea7ff3cae1c449
ronesala <ronesala@gmail.com>
2023-03-16 20:41:07 +0200

    Can count and output letter count in text file

--- LetterFrequencyDictionary.java
+++ LetterFrequencyDictionary.java
@@ -1,27 +1,53 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 // Klase, kas ielasa failu
 public class LetterFrequencyDictionary {
 
     // FileNotFound, ja nevar atrast failu
     public static void main(String[] args) throws IOException{
-        
+
         // Atver failu 
-        //File file = new File("./test1.txt");
-        FileReader file = new FileReader("./test1.txt");
-        
+        FileReader file = new FileReader("./test2.txt");
+
         // Ielasa failu
-        //Scanner scan = new Scanner(file);
-        BufferedReader br = new BufferedReader(file);
+        BufferedReader reader = new BufferedReader(file);
+
+        // Masiivs, kur tiks glabaati simboli un to skaits
+        HashMap<Character, Integer> LetterFrequency = new HashMap<Character, Integer>();
+
+        int charAsAnInteger = 0;    
 
         // Ielase pa simboliem
-        int c = 0;             
-        while((c = br.read()) != -1)
+        while((charAsAnInteger = reader.read()) != -1)
         {
-            char character = (char) c;          //no int uz char
-            System.out.println(character);      //izvade
+            if ( charAsAnInteger > 64 && charAsAnInteger < 91 || charAsAnInteger > 96 && charAsAnInteger < 123){
+
+                // Paarveido no int uz char
+                char character = (char) charAsAnInteger;
+
+                // Ja jau eksistee sarakstaa, tad palielina skaitu
+                if(LetterFrequency.containsKey(character)) {
+                    LetterFrequency.put(character, LetterFrequency.get(character) + 1);
+                }
+
+                // Ja pirmo reizi, tad skaits ir 1
+                else {
+                    LetterFrequency.put(character, 1);
+                }
+
+            }
+
+        }
+
+        // Izvade
+        for (Map.Entry<Character, Integer> entry : LetterFrequency.entrySet()) {
+
+            System.out.println(entry.getKey() + " - " + entry.getValue());
+
         }
 
         file.close();
